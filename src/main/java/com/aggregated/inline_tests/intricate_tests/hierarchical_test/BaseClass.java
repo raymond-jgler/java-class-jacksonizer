fefa9a1147f34b434881ae7dc287c90e5077575c
trgpnt <trungpnt0605@gmail.com>
2023-11-23 18:33:20 +0700

    _ tweaked tests to the hierarchical pattern

--- src/main/java/com/aggregated/inline_tests/intricate_tests/hierarchical_test/BaseClass.java
+++ src/main/java/com/aggregated/inline_tests/intricate_tests/hierarchical_test/BaseClass.java
@@ -1,11 +1,11 @@
 package com.aggregated.inline_tests.intricate_tests.hierarchical_test;
 
 import java.util.Map;
-import java.util.Set;
+import java.util.SortedSet;
 
 public abstract class BaseClass<T> {
     protected Integer x;
     protected String text;
     protected boolean y;
-    protected Map<String, Map<String, Set>> amIScary;
+    protected Map<String, Map<String, Map<SortedSet<Integer>, String>>> amIScary;
 }
