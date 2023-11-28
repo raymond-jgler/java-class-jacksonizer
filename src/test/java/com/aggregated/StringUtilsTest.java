47251f59a9d9f6ab8ecc241039a517d65ad3de1d
Hien Quan <hienq127@gmail.com>
2023-11-28 13:51:01 +0700

    Add unit test for isNoneBlank method of StringUtils

---
+++ src/test/java/com/aggregated/StringUtilsTest.java
@@ -0,0 +1,39 @@
+package com.aggregated;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringUtilsTest {
+
+    @Test
+    public void testIsNonBlankWithBlankString() {
+        assertFalse(StringUtils.isNoneBlank(""));
+    }
+
+    @Test
+    public void testIsNonBlankWittNullString(){
+        assertFalse(StringUtils.isNoneBlank(null));
+    }
+
+    @Test
+    public void testIsNoneBlankWithOnlyWhitespaceString() {
+        assertFalse(StringUtils.isNoneBlank("    "));
+    }
+
+    @Test
+    public void testIsNoneBlankWithLetterOrDigitString() {
+        assertTrue(StringUtils.isNoneBlank("abc123"));
+    }
+
+    @Test
+    public void testIsNoneBlankWithSpecialCharacters() {
+        assertTrue(StringUtils.isNoneBlank("abc!@#"));
+    }
+
+    @Test
+    public void testIsNoneBlankWithMixedCharacters() {
+        assertTrue(StringUtils.isNoneBlank("abc 123"));
+    }
+
+}
