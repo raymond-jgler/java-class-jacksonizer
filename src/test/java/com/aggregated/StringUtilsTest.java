a511ab3814dea8a879b4a23a600b8c75386b0b1a
trgpnt <trungpnt0605@gmail.com>
2023-12-01 19:02:13 +0700

    Setup test for this branch

--- src/test/java/com/aggregated/StringUtilsTest.java
+++ src/test/java/com/aggregated/StringUtilsTest.java
@@ -1,8 +1,12 @@
 package com.aggregated;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringUtilsTest {
 
@@ -34,6 +38,16 @@ public class StringUtilsTest {
     @Test
     public void testIsNoneBlankWithMixedCharacters() {
         assertTrue(StringUtils.isNoneBlank("abc 123"));
+    }
+    @Test
+    public void testDummy() {
+        int length = 10;
+        boolean useLetters = true;
+        boolean useNumbers = false;
+        String generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+
+        final Function<String, Boolean> testNoneBlank = StringUtils::isNoneBlank;
+        assertTrue(testNoneBlank.apply("@abc!@#"));
     }
 
 }
